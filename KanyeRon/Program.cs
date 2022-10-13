f2c4af9be6b2ba459731d98cbfcdb482a74b7290
Justin Lawson <jmlawson2002@gmail.com>
2022-10-13 14:22:35 -0400

    kanye and ron

---
+++ KanyeRon/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using KanyeRon;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using static System.Net.WebRequestMethods;
+
+var client = new HttpClient();
+
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine($"Kanye: {KanyeQuote}");
+    Console.WriteLine($"Ron: {RonResponse}");
+}
+    
---
+++ KanyeRon/QuoteGenerator.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Newtonsoft.Json.Linq;
+using System.Net.Http;
+using System.Runtime.Intrinsics.X86;
+
+namespace KanyeRon
+{
+    public class QuoteGenerator
+    {
+        public string KanyeQuote()
+        {
+            var client = new HttpClient();
+
+            var kanyeLink = "https://api.kanye.rest";
+
+            var kanyeResponse = client.GetStringAsync(kanyeLink).Result;
+
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+        public string RonResponse()
+        {
+            var client = new HttpClient();
+
+            var RonLink = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            var ronResponse = client.GetStringAsync(RonLink).Result;
+
+            var ronQuote = JArray.Parse(ronResponse).ToString().Replace('[', ' ').Replace(']', ' ').Trim();
+
+            return ronResponse;
+        }       
+    }
+}
